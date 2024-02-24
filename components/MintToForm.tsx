0c16f13982dfec93b5f387835b5d8639f0f3da71
Shivank Kapur <shivankkapur2004@gmail.com>
2024-02-25 03:31:31 +0530

    project done

--- components/MintToForm.tsx
+++ components/MintToForm.tsx
@@ -60,33 +60,42 @@ export const MintToForm: FC = () => {
       <div>
         <br />
         {publicKey ? (
-          <form onSubmit={mintTo} className="">
-            <label htmlFor="mint">Token Mint:</label>
+          <form onSubmit={mintTo} className="mt-4">
+            <label htmlFor="mint" className="block">
+              Token Mint:
+            </label>
             <input
               id="mint"
               type="text"
-              className=""
+              className="mt-1 block w-full rounded-md border-gray-300 shadow-sm focus:border-blue-500 focus:ring-blue-500"
               placeholder="Enter Token Mint"
               required
             />
-            <label htmlFor="recepient">Recepient:</label>
+            <label htmlFor="recepient" className="mt-4 block">
+              Recepient:
+            </label>
             <input
               id="recepient"
               type="text"
-              className=""
+              className="mt-1 block w-full rounded-md border-gray-300 shadow-sm focus:border-blue-500 focus:ring-blue-500"
               placeholder="Enter Recepient Public Key"
               required
             />
-            <label htmlFor="amount">Amount Tokens To Mint:</label>
+            <label htmlFor="amount" className="mt-4 block">
+              Amount Tokens To Mint:
+            </label>
             <input
               id="amount"
               type="text"
-              className=""
+              className="mt-1 block w-full rounded-md border-gray-300 shadow-sm focus:border-blue-500 focus:ring-blue-500"
               placeholder="Eg- 100"
               required
             />
 
-            <button type="submit" className="">
+            <button
+              type="submit"
+              className="mt-4 inline-flex items-center rounded-md border border-transparent bg-blue-500 px-4 py-2 font-semibold uppercase tracking-widest text-white transition hover:bg-blue-600 focus:border-blue-700 focus:outline-none focus:ring focus:ring-blue-200 active:bg-blue-700 disabled:opacity-25"
+            >
               Mint Tokens
             </button>
           </form>
@@ -95,10 +104,16 @@ export const MintToForm: FC = () => {
         )}
 
         {txSig ? (
-          <div>
-            <p>Token Balance: {balance}</p>
-            <p>View your transaction on: </p>
-            <a href={link()}>Solana Explorer</a>
+          <div className="mt-4">
+            <p className="text-xl font-bold text-black">
+              Token Balance: {balance}
+            </p>
+            <p className="text-xl font-bold text-black">
+              View your transaction on:{" "}
+            </p>
+            <a href={link()} className="text-xl font-semibold text-purple-600">
+              Solana Explorer
+            </a>
           </div>
         ) : null}
       </div>
